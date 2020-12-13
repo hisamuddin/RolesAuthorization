4d058fdef8ece5c89ce44f04ced89818c340b5f5
sabbu <sabbusiddique40@gmail.com>
2020-12-14 04:47:57 +0530

    Add project files.

---
+++ KiromicBioPharma.Application/TagHelper/RoleUsersTH.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiromicBioPharma.Application.TagHelper
+{
+    [HtmlTargetElement("td", Attributes = "i-role")]
+    public class RoleUsersTH : TagHelperComponent
+    {
+        private UserManager<IdentityUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleUsersTH(UserManager<IdentityUser> usermgr, RoleManager<IdentityRole> rolemgr)
+        {
+            userManager = usermgr;
+            roleManager = rolemgr;
+        }
+
+        [HtmlAttributeName("i-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            IdentityRole role = await roleManager.FindByIdAsync(Role);
+            if (role != null)
+            {
+                foreach (var user in userManager.Users)
+                {
+                    if (user != null && await userManager.IsInRoleAsync(user, role.Name))
+                        names.Add(user.UserName);
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "No Users" : string.Join(", ", names));
+        }
+    }
+}
